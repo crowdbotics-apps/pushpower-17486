b3c01fcbea8d7b3bb4b1d4c39b082129f7966988
Crowdbotics <team@crowdbotics.com>
2020-05-28 01:50:10 +0000

    Update models

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import CustomText
+from .models import CustomText, HomePage
 
 admin.site.register(CustomText)
+admin.site.register(HomePage)
 
 # Register your models here.
